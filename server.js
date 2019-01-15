12ca9c4dd47a039db9c3dd770caf7524772c1112
Tolumide <tolumideshopein@gmail.com>
2019-01-15 08:58:19 +0100

    [Feature #163198448] Add server and test file

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, ()=> console.log(`listening on port ${port}`));
