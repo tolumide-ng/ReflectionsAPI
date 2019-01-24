2cff89a598a2ffd3b0b98c2a8034482c0d1bec33
Tolumide <tolumideshopein@gmail.com>
2019-01-24 20:49:48 +0100

    [Chore #163308152] server.js moved from root dir to src

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import reflectionRoutes from './src/Routes/reflectionsRoutes';
+import '@babel/polyfill';
 import bodyParser from 'body-parser';
+import reflectionRoutes from './Routes/reflectionsRoutes';
 
 const app = express();
 
 app.use(express.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use('/api/v1/reflections', reflectionRoutes);
